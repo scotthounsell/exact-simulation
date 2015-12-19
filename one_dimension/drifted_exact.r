2f90a53e65057557c71a9481018cd7a461752d22
Scott Hounsell <Scott.Hounsell@baruchmail.cuny.edu>
2015-12-19 12:08:36 -0500

    Clean some comments

--- one_dimension/drifted_exact.r
+++ one_dimension/drifted_exact.r
@@ -44,7 +44,7 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
         # For plotting
         plot_num <- 1
         if (num_paths_to_plot==TRUE) num_paths_to_plot <- 100 # Default value
-        num_paths_to_plot <- min(num_paths_to_plot, num_of_paths)
+        num_paths_to_plot <- min(num_paths_to_plot, num_of_paths) # Cannot plot more than we have
         if (num_paths_to_plot) {
             plot_amount <- round(table(all_N_T)/length(all_N_T) * num_paths_to_plot) # Get the number of paths to plot for each unique N_T
             line_colors <- rainbow(num_paths_to_plot)
@@ -73,7 +73,7 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
                 #     g (function): A function taking X_T and returning a scalar
                 # Returns:
                 #     numeric: An estimator in expectation for g(X_T)
-                (g(XhatT) - if(N_T > 0) g(XhatTNT) else 0) * g_multiple
+                (g(XhatT) - if (N_T > 0) g(XhatTNT) else 0) * g_multiple
 
             # Hold all dt and t paths of length N_T+2 in a matrix.
             # Each row of the matrix is a path. Time spans columns.
@@ -85,7 +85,7 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
 
             # Simulate the process Yhat using the exact method on the Lamperti transformation of the process X
 
-            # Initialize Y0. More efficient to just initialize all values as Y0
+            # Initialize Yhat with Y0 in first column (more efficient to just initialize all values as Y0)
             Yhat <- matrix(Y0, nr=nrow(dt), nc=ncol(dt))
 
             # Step through time
@@ -94,7 +94,7 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
 
             # Convert the process Yhat back into the process Xhat (we only need the last two elements at most)
             XhatT <- convert_y_to_x(Yhat[,ncol(Yhat)])
-            if (N_T > 0) XhatTNT <- convert_y_to_x(Yhat[,ncol(Yhat)-1])
+            if (N_T>0) XhatTNT <- convert_y_to_x(Yhat[,ncol(Yhat)-1])
 
             # Calculates the product from k=1..N_T of malliavin_weight(k)
             # Defaults to 1 if no arrivals before T
@@ -106,7 +106,7 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
             # Plot paths
             if (num_paths_to_plot){
                 plots <- plot_amount[plot_num]
-                if (plot_num == 1){
+                if (plot_num == 1){ # First plot initializes the figure
                     matplot(t(t[1:plots,]), t(convert_y_to_x(Yhat[1:plots,])), main=sprintf('Generated paths (first %i paths) - Exact scheme', num_paths_to_plot), xlab='t', ylab='X_t', type='l', lty=1)
                 } else {
                     matplot(t(t[1:plots,]), t(convert_y_to_x(Yhat[1:plots,])), type='l', lty=1, add=TRUE)
@@ -123,10 +123,10 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
             # The first row is the sum of the prices across paths.
             # The second row is the sum of the squared prices across paths.
             # Each column represents a strike
-#~             if (is.vector(psi_matrix)) return(rbind(psi_matrix, psi_matrix*psi_matrix)) #If only one path
-#~             return(rbind(colSums(psi_matrix), colSums(psi_matrix*psi_matrix))) # Sum paths for each strike
+            # if (is.vector(psi_matrix)) return(rbind(psi_matrix, psi_matrix*psi_matrix)) #If only one path
+            # return(rbind(colSums(psi_matrix), colSums(psi_matrix*psi_matrix))) # Sum paths for each strike
 
-            # TEST This may be easier to understand, just return the price matrix
+            # This method may be slower, but easier to understand: just return the price matrix
             return(vapply(strikes, function(K) psi(g_maker(K)), FUN.VALUE=numeric(length(XhatT))))
         }
 
@@ -134,12 +134,12 @@ Drifted_Exact <- function(num_of_paths, beta, mu, sigma0=1, Texp=1, X0=0, conver
         #return(rowSums(vapply(sort(unique(all_N_T)), sum_prices, FUN.VALUE=strikes)) / num_of_paths)
 
         # For each number of defaults, we have a matrix with a sum row (1) and a sum squared row (2) with a column for each strike
-#~         sum_and_squared <- Reduce('+', lapply(sort(unique(all_N_T)), sum_prices))
-#~         # Print the variance of the prices for each strike
-#~         print(data.frame(K=strikes, Variance=1/(num_of_paths-1) *(sum_and_squared[2,] - 1/num_of_paths * (sum_and_squared[1,])^2)))
-#~         return(sum_and_squared[1,]/num_of_paths) # Return the mean prices for each strike
+        # sum_and_squared <- Reduce('+', lapply(sort(unique(all_N_T)), sum_prices))
+        # Print the variance of the prices for each strike
+        # print(data.frame(K=strikes, Variance=1/(num_of_paths-1) *(sum_and_squared[2,] - 1/num_of_paths * (sum_and_squared[1,])^2)))
+        # return(sum_and_squared[1,]/num_of_paths) # Return the mean prices for each strike
 
-        # TEST This may be easier to understand, if we were to just return the prices (psi_matrix)
+        # This method may be slower, but easier to understand: just return the prices (psi_matrix)
         price_matrix <- do.call(rbind, lapply(sort(unique(all_N_T)), sum_prices)) # Concatenate the price matrices for each N_T
         print(data.frame(K=strikes, Variance=diag(var(price_matrix)))) # Diagonals of cov mtx are column variances
         return(colMeans(price_matrix))
